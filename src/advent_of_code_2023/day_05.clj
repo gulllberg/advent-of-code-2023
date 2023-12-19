4ea5ca5b0fab53061a5d0212554017277b0f5b4a
Daniel Gullberg <daniel@gullberg.dev>
2023-12-19 16:14:57 +0100

    New day 5 wip

--- src/advent_of_code_2023/day_05.clj
+++ src/advent_of_code_2023/day_05.clj
@@ -38,19 +38,71 @@
               seeds)
          (apply min))))
 
-(defn do-seed-partition
-  [seed-partition mapping-fns]
-  (let [start (first seed-partition)
-        length (second seed-partition)
-        end (+ start length)]
-    (loop [seed start
-           min-location ##Inf]
-      (if (>= seed end)
-        min-location
-        (recur (inc seed) (min min-location (reduce (fn [a mapping-fn]
-                                                      (mapping-fn a))
-                                                    seed
-                                                    mapping-fns)))))))
+;; e exclusive, 1,5 ->1,2,3,4
+(defn interval-intersection
+  {:test (fn []
+           (is= (interval-intersection [1 5] [6 9]) [nil [1 5]])
+           (is= (interval-intersection [1 5] [-1 0]) [nil [1 5]])
+           (is= (interval-intersection [3 5] [1 5]) [[3 5]])
+           (is= (interval-intersection [3 5] [4 6]) [[4 5] [3 4]])
+           (is= (interval-intersection [3 5] [1 4]) [[3 4] [4 5]])
+           (is= (interval-intersection [1 5] [2 4]) [[2 4] [1 2] [4 5]]))}
+  [[s1 e1] [s2 e2]]
+  ; [[intersecting-interval] [remaining-intervals (1 or 2)]]
+  (cond
+    ; i2 smaller than i2
+    (> s1 e2)
+    [nil [s1 e1]]
+
+    ; i1 smaller than i2
+    (> s2 e1)
+    [nil [s1 e1]]
+
+    ; i1 entirely within i2
+    (<= s2 s1 e1 e2)
+    [[s1 e1]]
+
+    ; i1 intersects "to the left"
+    (<= s1 s2 e1 e2)
+    [[s2 e1] [s1 s2]]
+
+    ; i1 intersects "to the right"
+    (<= s2 s1 e2 e1)
+    [[s1 e2] [e2 e1]]
+
+    ; i2 entirely within i1 - split it in 3
+    :else
+    [[s2 e2] [s1 s2] [e2 e1]]))
+
+(defn get-mapping-fn-b
+  [mapping]
+  (let [[dest src len] (->> (re-seq #"\d+" mapping)
+                            (map read-string))
+        [s2 e2] [src (+ src len)]]
+    (fn [[s1 e1]]
+      (let [[intersection & new-intervals] (interval-intersection [s1 e1] [s2 e2])]
+        (if intersection
+          (concat (mapv + intersection (into [] (repeat 2 (- dest src)))) new-intervals)
+          (concat [nil] new-intervals))))))
+
+(defn combine-mapping-fns-b
+  [almanac]
+  (let [parts (clojure.string/split-lines almanac)
+        fns (mapv get-mapping-fn-b (rest parts))]
+    (fn [[s e]]
+      (let [[mapped-intervals unmapped-intervals] (reduce (fn [[mapped-intervals remaining-intervals] mapping-fn]
+                                                            (if (empty? remaining-intervals)
+                                                              (reduced mapped-intervals)
+                                                              (reduce (fn [[mapped-intervals remaining-intervals] [s1 e1]]
+                                                                        (let [[mapping & new-intervals] (mapping-fn [s1 e1])]
+                                                                          (if mapping
+                                                                            [(conj mapped-intervals mapping) (concat remaining-intervals new-intervals)]
+                                                                            [mapped-intervals (concat remaining-intervals new-intervals)])))
+                                                                      [mapped-intervals []]
+                                                                      remaining-intervals)))
+                                                          [[] [[s e]]]
+                                                          fns)]
+        (concat mapped-intervals unmapped-intervals)))))
 
 (defn solve-b
   {:test (fn []
@@ -60,15 +112,19 @@
         seed-partitions (->> (re-seq #"\d+" (first almanac))
                              (map read-string)
                              (partition 2))
-        mapping-fns (map combine-mapping-fns (rest almanac))]
-    (println "number of seed partitions")
-    (println (count seed-partitions))
-    (reduce (fn [min-location seed-partition]
-              (println "starting seed partition")
-              (println seed-partition)
-              (min min-location (do-seed-partition seed-partition mapping-fns)))
-            ##Inf
-            seed-partitions)))
+        mapping-fns (map combine-mapping-fns-b (rest almanac))]
+    (->> (map (fn [[s l]]
+                (->> mapping-fns
+                     (reduce (fn [intervals mapping-fn]
+                               (reduce (fn [intervals interval]
+                                         (concat intervals (mapping-fn interval)))
+                                       []
+                                       intervals))
+                             [[s (+ s l)]])
+                     (map first)
+                     (apply min)))
+              seed-partitions)
+         (apply min))))
 
 
 (comment
@@ -77,7 +133,6 @@
 
   (time (solve-b input))
   ;; 11611182
-  ;; "Elapsed time: 2.7138193503851E7 msecs"
-  ;; Oops...
+  ;;
   )
 
